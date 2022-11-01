845a3896961f64768d5a7c7bfbc78f152a0f9348
Deeksha Dhuny <deekshadhuny1107@gmail.com>
2022-11-01 15:09:32 +0400

    adding stuff into website

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import home_view, list_view
+from .views import home_view, list_view, CreateView, DeleteView
 
 urlpatterns = [
     path('blog/', home_view, name="blog-home"),
     path('blog/list/', list_view, name="blog-list"),
-]
+    path('blog/add/', CreateView.as_view(), name="blog-add"),
+    path('blog/del/', DeleteView.as_view(), name="blog-del"),
+ ]
+
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import BlogItem
+from django.views.generic import TemplateView
+from django.http.response import HttpResponse
 
 def home_view(request, *args, **kwargs):
 	return redirect('blog-list')
@@ -9,6 +11,81 @@ def list_view(request, *args, **kwargs):
 	     'blogs': BlogItem.objects.all(),
 	}
 	return render(request, 'blog/list.html', context=ctx)
+
+
+
+class CreateView(TemplateView):
+	
+	template_name = 'blog/create.html'
+
+	def get(self,request, *args, **kwargs):
+		return render(request, 'blog/create.html', context={})
+
+
+	def post(self,request, *args, **kwargs):
+		    title = request.POST['title']
+		    body = request.POST['body']
+		    blog_item = BlogItem()
+		    blog_item.title = title
+		    blog_item.body = body
+		    blog_item.save()
+		    return redirect('blog-list')
+
+
+class DeleteView(TemplateView):
+
+	template_name = 'blog/delete.html'
+	
+	def get(self,request, *args, **kwargs):
+		if 'id' not in request.GET:
+			return HttpResponse('<h1>You\'ve been a naughty boy or girl</h1>', status=400)
+		
+		try:
+
+			id = int (request.GET['id'])
+
+		except (ValueError,TypeError):
+
+			return HttpResponse('<h1>You are guessing!</h1>', status=400)
+
+		try:
+				
+			self.blog_item = BlogItem.objects.get(id=id)
+		
+		except BlogItem.DoesNotExist:
+			
+			return HttpResponse('<h1>Well that\'s not even there</h1>', status=400)	
+		return super().get(request, *args, **kwargs)
+
+	def post(self,  request, *args, **kwargs):	
+		if 'delete' not in request.POST:
+			return HttpResponse('<h1>Intruder!</h1>', status=400)
+
+		if request.POST['delete'] != 'YES':
+			return HttpResponse('<h1>Intruder!</h1>', status=400)	
+
+		if 'id' not in request.POST:
+			return HttpResponse('<h1>Intruder!</h1>', status=400)	
+
+		try:
+				
+			blog_item = BlogItem.objects.get(id=int(request.POST['id']))
+		
+		except (ValueError, TypeError, BlogItem.DoesNotExist):
+			
+			return HttpResponse('<h1>Not found</h1>', status=400)			
+
+		blog_item.delete()
+		return redirect('blog-list')	
+
+
+	def get_context_data(self, *args, **kwargs):
+		ctx = super().get_context_data(*args, **kwargs)	
+		ctx['blog_item'] = self.blog_item
+		print(ctx)
+		return ctx
+
+
 
 
 
